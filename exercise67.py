f27f6c5c526ba59ef74fcfcc480c9bdc8076aa5b
Vadims06 <vadim.semenov06@gmail.com>
2016-08-03 03:48:02 -0700

    resolve errors

--- exercise67.py
+++ exercise67.py
@@ -1,10 +1,10 @@
 import yaml
-import yson
+import json
 from pprint import pprint as pp
 with open('some_file.json') as f:
 	new_list_j = json.load(f)
 with open('some_file.yml') as f:
-	new_list_y = jaml.load(f)
+	new_list_y = yaml.load(f)
 pp('JSON:')
 pp(new_list_j)
 pp('YAML:')
